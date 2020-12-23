f6852f81b136f48fa5e866abfc978a4abed85ad9
JonaOmara <49917566+JonaOmara@users.noreply.github.com>
2020-12-24 02:33:45 +0300

    html edit

--- App.py
+++ App.py
@@ -6,21 +6,23 @@ import dill
 app = Flask(__name__)
 
 
+# function to direct the main page to index
 @app.route("/")
 def main():
     return redirect("/index")
 
 
 @app.route('/index', methods=['GET'])
-def index():
+def index():  # function to load html page on index
     return render_template('/index.html')
 
 
 @app.route('/about')
-def about():
-    return 'This is about Sentiment Analysis'
+def about():  # function to manage the about page
+    return 'This is about Sentiment Analysis web app'
 
 
+# function to make predictions and return results on the about page
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'GET':
